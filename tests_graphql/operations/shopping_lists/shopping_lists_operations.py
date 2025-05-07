80aed74256b5618b55a1fa9005d91ead9b435972
Andrew Orlov <andrew.orlov@outlook.com>
2025-05-07 07:24:37 +0200

    feat: implement graphql create shopping list

---
+++ tests_graphql/operations/shopping_lists/shopping_lists_operations.py
@@ -0,0 +1,30 @@
+from gql import Client
+from graphql_client.types.wishlist_type import WishlistType
+from graphql_client.mutations.create_wishlist import CreateWishlistMutation
+from graphql_client.types.input_create_wishlist_type import InputCreateWishlistType
+from tests_graphql.operations.shopping_lists.fragments.shopping_list_fragment import SHOPPING_LIST_FRAGMENT
+from graphql_client.types.input_remove_wishlist_type import InputRemoveWishlistType
+from graphql_client.mutations.remove_wishlist import RemoveWishlistMutation
+
+
+class ShoppingListsOperations:
+    def __init__(self, graphql_client: Client):
+        self.graphql_client = graphql_client
+
+    def create_shopping_list(self, payload: InputCreateWishlistType) -> WishlistType:
+        create_wishlist_mutation = CreateWishlistMutation(self.graphql_client)
+
+        variables = {"command": payload}
+
+        result = create_wishlist_mutation.execute(variables=variables, return_fields=SHOPPING_LIST_FRAGMENT)
+
+        return result
+
+    def remove_shopping_list(self, payload: InputRemoveWishlistType) -> None:
+        remove_wishlist_mutation = RemoveWishlistMutation(self.graphql_client)
+
+        variables = {"command": payload}
+
+        result = remove_wishlist_mutation.execute(variables=variables, return_fields=None)
+
+        return result
---
+++ tests_graphql/tests/test_graphql_create_shopping_list.py
@@ -0,0 +1,45 @@
+import allure, os
+from tests_graphql.operations.user.user_operations import UserOperations
+from tests_graphql.operations.shopping_lists.shopping_lists_operations import ShoppingListsOperations
+from tests_graphql.test_data.test_culture import TEST_CULTURE
+from tests_graphql.test_data.test_currency import TEST_CURRENCY
+from tests_graphql.test_data.test_user import TEST_ADMIN_USER
+
+
+@allure.title("Create shopping list (GraphQL)")
+def test_create_shopping_list(config, user_service, graphql_client):
+    print(f"{os.linesep}Running test to create shopping list...", end=" ")
+
+    user_operations = UserOperations(graphql_client)
+    shopping_lists_operations = ShoppingListsOperations(graphql_client)
+
+    user_service.sign_in(TEST_ADMIN_USER["username"], TEST_ADMIN_USER["password"])
+
+    user = user_operations.get_user()
+
+    shopping_list = shopping_lists_operations.create_shopping_list(
+        payload={
+            "userId": user["id"],
+            "storeId": config["store_id"],
+            "listName": "[E2E test] Test shopping list",
+            "cultureName": TEST_CULTURE["en-US"],
+            "currencyCode": TEST_CURRENCY["USD"],
+            "scope": "Private",
+        }
+    )
+
+    # Test teardown
+
+    shopping_lists_operations.remove_shopping_list(
+        payload={
+            "listId": shopping_list["id"],
+        }
+    )
+
+    user_service.sign_out()
+
+    assert shopping_list["id"] is not None
+    assert shopping_list["storeId"] == config["store_id"]
+    assert shopping_list["customerId"] == user["id"]
+    assert shopping_list["itemsCount"] == 0
+    assert shopping_list["scope"] == "Private"
