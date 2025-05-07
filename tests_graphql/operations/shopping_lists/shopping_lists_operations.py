7b2011677ecc0b0548fc44b34dd3ac2d6051d723
Andrew Orlov <andrew.orlov@outlook.com>
2025-05-07 07:46:28 +0200

    feat: implement graphql get shopping lists test

--- tests_graphql/operations/shopping_lists/shopping_lists_operations.py
+++ tests_graphql/operations/shopping_lists/shopping_lists_operations.py
@@ -5,6 +5,8 @@ from graphql_client.types.input_create_wishlist_type import InputCreateWishlistT
 from tests_graphql.operations.shopping_lists.fragments.shopping_list_fragment import SHOPPING_LIST_FRAGMENT
 from graphql_client.types.input_remove_wishlist_type import InputRemoveWishlistType
 from graphql_client.mutations.remove_wishlist import RemoveWishlistMutation
+from graphql_client.types.wishlist_connection import WishlistConnection
+from graphql_client.queries.wishlists import WishlistsQuery
 
 
 class ShoppingListsOperations:
@@ -17,6 +19,21 @@ class ShoppingListsOperations:
         variables = {"command": payload}
 
         result = create_wishlist_mutation.execute(variables=variables, return_fields=SHOPPING_LIST_FRAGMENT)
+
+        return result
+
+    def get_shopping_lists(
+        self, store_id: str, user_id: str, currency_code: str, culture_name: str
+    ) -> WishlistConnection:
+        wishlists_query = WishlistsQuery(self.graphql_client)
+
+        variables = {"storeId": store_id, "userId": user_id, "currencyCode": currency_code, "cultureName": culture_name}
+
+        return_fields = """
+            totalCount
+        """
+
+        result = wishlists_query.execute(variables=variables, return_fields=return_fields)
 
         return result
 
