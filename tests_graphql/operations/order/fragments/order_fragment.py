6b3469083b8a387ef59c8060706b950ee433ecb4
Andrew Orlov <andrew.orlov@outlook.com>
2025-05-14 09:30:55 +0200

    feat: implement order sorting graphql test

--- tests_graphql/operations/order/fragments/order_fragment.py
+++ tests_graphql/operations/order/fragments/order_fragment.py
@@ -2,9 +2,13 @@ ORDER_FRAGMENT = f"""
     id
     number
     customerId
+    createdDate
     items {{
         id
         productId
         quantity
+    }}
+    total {{
+        amount
     }}
 """
---
+++ tests_graphql/tests/test_graphql_sort_orders.py
@@ -0,0 +1,72 @@
+import allure, os
+from tests_graphql.operations.user.user_operations import UserOperations
+from tests_graphql.operations.order.order_operations import OrderOperations
+from tests_graphql.test_data.test_user import TEST_PERMANENT_USER
+from tests_graphql.test_data.test_culture import TEST_CULTURE
+
+
+@allure.title("Sort orders by date (GraphQL)")
+def test_sort_orders_by_date(config, user_service, graphql_client):
+    print(f"{os.linesep}Running test to sort orders by date...", end=" ")
+
+    user_operations = UserOperations(graphql_client)
+    order_operations = OrderOperations(graphql_client)
+
+    user_service.sign_in(TEST_PERMANENT_USER["username"], TEST_PERMANENT_USER["password"])
+
+    user = user_operations.get_user()
+
+    search_orders_result_created_date_desc = order_operations.get_orders(
+        user_id=user["id"],
+        culture_name=TEST_CULTURE["en-US"],
+    )
+
+    order_dates_desc = [order["createdDate"] for order in search_orders_result_created_date_desc["items"]]
+    is_sorted_desc = all(order_dates_desc[i] >= order_dates_desc[i + 1] for i in range(len(order_dates_desc) - 1))
+
+    search_orders_result_created_date_asc = order_operations.get_orders(
+        user_id=user["id"],
+        culture_name=TEST_CULTURE["en-US"],
+        sort="createdDate:asc",
+    )
+
+    order_dates_asc = [order["createdDate"] for order in search_orders_result_created_date_asc["items"]]
+    is_sorted_asc = all(order_dates_asc[i] <= order_dates_asc[i + 1] for i in range(len(order_dates_asc) - 1))
+
+    user_service.sign_out()
+
+    assert is_sorted_desc, "Orders are not sorted by date in descending order"
+    assert is_sorted_asc, "Orders are not sorted by date in ascending order"
+
+
+@allure.title("Sort orders by total amount (GraphQL)")
+def test_sort_orders_by_total_amount(config, user_service, graphql_client):
+    print(f"{os.linesep}Running test to sort orders by total amount...", end=" ")
+
+    user_operations = UserOperations(graphql_client)
+    order_operations = OrderOperations(graphql_client)
+
+    user_service.sign_in(TEST_PERMANENT_USER["username"], TEST_PERMANENT_USER["password"])
+
+    user = user_operations.get_user()
+
+    search_orders_result_total_amount_desc = order_operations.get_orders(
+        user_id=user["id"], culture_name=TEST_CULTURE["en-US"], sort="total:desc"
+    )
+
+    order_totals_desc = [order["total"]["amount"] for order in search_orders_result_total_amount_desc["items"]]
+    is_sorted_desc = all(order_totals_desc[i] >= order_totals_desc[i + 1] for i in range(len(order_totals_desc) - 1))
+
+    search_orders_result_order_total_amount_asc = order_operations.get_orders(
+        user_id=user["id"],
+        culture_name=TEST_CULTURE["en-US"],
+        sort="total:asc",
+    )
+
+    order_totals_asc = [order["total"]["amount"] for order in search_orders_result_order_total_amount_asc["items"]]
+    is_sorted_asc = all(order_totals_asc[i] <= order_totals_asc[i + 1] for i in range(len(order_totals_asc) - 1))
+
+    user_service.sign_out()
+
+    assert is_sorted_desc, "Orders are not sorted by date in descending order"
+    assert is_sorted_asc, "Orders are not sorted by date in ascending order"
