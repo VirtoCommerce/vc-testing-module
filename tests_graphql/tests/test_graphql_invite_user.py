0be8e2b3ee02a65b76e4922d8520e9a2787cacc7
Andrew Orlov <andrew.orlov@outlook.com>
2025-05-29 18:03:41 +0200

    feat: implement invite user graphql test

---
+++ tests_graphql/tests/test_graphql_invite_user.py
@@ -0,0 +1,59 @@
+import allure, os
+from tests_graphql.operations.user.user_operations import UserOperations
+from tests_graphql.operations.contact.contact_operations import ContactOperations
+from tests_graphql.test_data.test_user import TEST_PERMANENT_CORPORATE_USER
+
+
+@allure.feature("Invite user (GraphQL)")
+def test_invite_user(config, user_service, graphql_client):
+    print(f"{os.linesep}Running test to invite user...", end=" ")
+
+    user_operations = UserOperations(graphql_client)
+    contact_operations = ContactOperations(graphql_client)
+
+    user_service.sign_in(TEST_PERMANENT_CORPORATE_USER["username"], TEST_PERMANENT_CORPORATE_USER["password"])
+
+    user = user_operations.get_user()
+
+    invitation_result = contact_operations.invite_user(
+        payload={
+            "storeId": config["store_id"],
+            "organizationId": user["contact"]["organizationId"],
+            "emails": ["e2e-test-corporate-temp@test.com"],
+            "message": "You are invited to join the organization",
+            "roleIds": ["org-employee"],
+        }
+    )
+
+    invited_contact = contact_operations.fetch_organization_contacts(
+        organization_id=user["contact"]["organizationId"],
+        user_id=user["id"],
+        search_phrase="e2e-test-corporate-temp@test.com",
+    )["contacts"]["items"][0]
+
+    # Test teardown
+
+    contact_operations.remove_contact_from_organization(
+        payload={
+            "contactId": invited_contact["id"],
+            "organizationId": user["contact"]["organizationId"],
+        }
+    )
+
+    contact_operations.delete_contact(
+        payload={
+            "contactId": invited_contact["id"],
+        }
+    )
+
+    user_operations.delete_users(
+        payload={
+            "userNames": ["e2e-test-corporate-temp@test.com"],
+        }
+    )
+
+    user_service.sign_out()
+
+    assert invitation_result["succeeded"] == True, "User was not invited"
+    assert invited_contact is not None, "Invited contact was not found"
+    assert invited_contact["status"] == "Invited", "Invited contact status is not Invited"
