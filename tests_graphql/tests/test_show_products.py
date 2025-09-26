ea3565595c77f4a56321efffe1b29a7d1a7e292a
Andrew Orlov <andrew.orlov@outlook.com>
2025-09-26 10:50:12 +0200

    fix: refactor show products test

--- tests_graphql/tests/test_show_products.py
+++ tests_graphql/tests/test_show_products.py
@@ -2,7 +2,6 @@ import json
 from typing import Any, Dict
 
 from fixtures.graphql_client import GraphQLClient
-from graphql_operations.catalog.categories_operations import CategoriesOperations
 from graphql_operations.catalog.products_operations import ProductsOperations
 from graphql_operations.user.user_operations import UserOperations
 
@@ -13,7 +12,6 @@ def test_show_products(
     print("================ Products ==================")
 
     user_operations = UserOperations(graphql_client)
-    categories_operations = CategoriesOperations(graphql_client)
     products_operations = ProductsOperations(graphql_client)
 
     currency = dataset["currencies"][0]["code"]
@@ -21,20 +19,12 @@ def test_show_products(
     catalog = dataset["catalogs"][0]
     user = user_operations.get_me()
 
-    category = categories_operations.get_category(
-        store_id=config["store_id"],
-        user_id=user["id"],
-        currency_code=currency,
-        culture_name=culture,
-        id="category-acme-laptops",
-    )
-
     products_all_response = products_operations.get_products(
         store_id=config["store_id"],
         user_id=user["id"],
         currency_code=currency,
         culture_name=culture,
-        filter=f"category.subtree:{catalog['id']}/{category['id']}",
+        filter=f"category.subtree:{catalog['id']}",
     )
 
     for product in products_all_response["items"]:
