7f4076c562aeac8f2db3d0bd00947fdf257e3979
Andrew Orlov <Andrew.Orlov@virtoworks.com>
2025-12-18 10:06:53 +0200

    chore: fix config fixture

--- fixtures/config.py
+++ fixtures/config.py
@@ -1,10 +1,14 @@
+import os
+
 import pytest
 from dotenv import dotenv_values
 
 
 class Config:
     def __init__(self):
-        self._config = dotenv_values(encoding="utf-8")
+        file_values = dotenv_values(encoding="utf-8")
+        env_vaues = dict(os.environ)
+        self._config = {**file_values, **env_vaues}
 
     def __getitem__(self, key: str):
         return self._config[key]
