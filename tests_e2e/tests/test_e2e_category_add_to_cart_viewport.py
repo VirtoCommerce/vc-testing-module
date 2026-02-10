7a607eacf4f5d266011f733162384bb55477ba01
Andrew Orlov <Andrew.Orlov@virtoworks.com>
2026-02-10 21:47:39 +0200

    fix: restore test to run in CI

--- tests_e2e/tests/test_e2e_category_add_to_cart_viewport.py
+++ tests_e2e/tests/test_e2e_category_add_to_cart_viewport.py
@@ -22,36 +22,42 @@ def test_e2e_category_add_to_cart_component_viewport(
     page.set_viewport_size({"width": 1920, "height": 1080})
 
     category = dataset["categories"][0]
-    product = dataset["products"][1]
+    product = dataset["products"][14]
 
     category_page = CategoryPage(config, page, category["seoInfos"][0]["semanticUrl"])
     category_page.navigate()
 
     category_page.view_switcher.switch_category_view("grid")
 
-    product_card = category_page.get_product_card_by_sku(product["code"])
+    product_card = category_page.scroll_to_product_card(product["code"])
 
-    expect(product_card.element).to_be_visible(), "Product card is not visible"
+    assert product_card is not None, "Product card is not found"
+    expect(product_card.element, "Product card is not visible").to_be_visible()
 
     if config["PRODUCT_QUANTITY_CONTROL"] == "stepper":
         expect(
-            product_card.quantity_stepper_component.element
-        ).to_be_visible(), "Quantity stepper component is not visible"
+            product_card.quantity_stepper_component.element,
+            "Quantity stepper component is not visible",
+        ).to_be_visible()
 
     if config["PRODUCT_QUANTITY_CONTROL"] == "button":
         expect(
-            product_card.add_to_cart_component.add_to_cart_text_button
-        ).to_be_visible(), "Add to cart text button is not visible"
+            product_card.add_to_cart_component.add_to_cart_text_button,
+            "Add to cart text button is not visible",
+        ).to_be_visible()
         expect(
-            product_card.add_to_cart_component.add_to_cart_icon_button
-        ).not_to_be_visible(), "Add to cart icon button is visible"
+            product_card.add_to_cart_component.add_to_cart_icon_button,
+            "Add to cart icon button is visible",
+        ).not_to_be_visible()
 
     page.set_viewport_size({"width": 800, "height": 600})
 
     if config["PRODUCT_QUANTITY_CONTROL"] == "button":
         expect(
-            product_card.add_to_cart_component.add_to_cart_text_button
-        ).not_to_be_visible(), "Add to cart text button is visible"
+            product_card.add_to_cart_component.add_to_cart_text_button,
+            "Add to cart text button is visible",
+        ).not_to_be_visible()
         expect(
-            product_card.add_to_cart_component.add_to_cart_icon_button
-        ).to_be_visible(), "Add to cart icon button is not visible"
+            product_card.add_to_cart_component.add_to_cart_icon_button,
+            "Add to cart icon button is not visible",
+        ).to_be_visible()
